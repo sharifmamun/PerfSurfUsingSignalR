c54295a859cf7127ad2cb70921608b88e2c535df
Sharif Mamun <sharif.mamun@gmail.com>
2017-08-04 11:09:03 -0700

    Injected SignalR Hub

---
+++ PerfSurfUsingSignalR/Hubs/PerfHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace PerfSurfUsingSignalR.Hubs
+{
+    public class PerfHub : Hub
+    {
+        public void Send(string message)
+        {
+            if (!string.IsNullOrEmpty(message.Trim()))
+            {
+                Clients.All.newMessage(
+                    Context.User.Identity.Name + " says " + message
+                );
+            }
+        }
+    }
+}
---
+++ PerfSurfUsingSignalR/Scripts/perfSurf.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    var perfHub = $.connection.perfHub;
+    $.connection.hub.logging = true;
+    $.connection.hub.start();
+
+    perfHub.client.newMessage = function (message) {
+        if (message) {
+            model.addMessage(message);
+        } else {
+            console.log("Message can not be empty");
+        }
+    };
+
+    var Model = function () {
+        var self = this;
+
+        self.message = ko.observable("");
+        self.messages = ko.observableArray();
+    };
+
+    Model.prototype = {
+
+        sendMessage: function () {
+            var self = this;
+            perfHub.server.send(self.message());
+            self.message("");
+        },
+
+        addMessage: function (message) {
+            var self = this;
+
+            self.messages.push(message);
+        }
+    };
+
+    var model = new Model();
+
+    $(function () {
+        ko.applyBindings(model);
+    });
+
+}());
